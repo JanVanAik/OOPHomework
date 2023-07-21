f04aba974142205fff8e8f394b3d66a00f47f2f5
JanVanAik <ar.orlovskiy@gmail.com>
2023-07-21 11:49:47 +0300

    task 6

--- src/main/java/Lesson_6/work3/CasualGreeter.java
+++ src/main/java/Lesson_6/work3/CasualGreeter.java
@@ -1,6 +1,6 @@
 package Lesson_6.work3;
 
-public class CasualGreeter extends Greeter{
+public class CasualGreeter extends Greeter {
     @Override
     public String greet() {
         return "Sup bro?";
--- src/main/java/Lesson_6/work3/IntimateGreeter.java
+++ src/main/java/Lesson_6/work3/IntimateGreeter.java
@@ -1,6 +1,6 @@
 package Lesson_6.work3;
 
-public class IntimateGreeter extends Greeter{
+public class IntimateGreeter extends Greeter {
     @Override
     public String greet() {
         return "Hello Darling!";
